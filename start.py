dd7ca902145e39d6554998a3cbd727c22f0d485e
Trevor <supershadowplay@gmail.com>
2019-10-24 14:33:53 -0400

    Added start.py and token.txt

---
+++ start.py
@@ -0,0 +1,172 @@
+import asyncio
+import discord
+import youtube_dl
+import time
+
+from discord.ext import commands
+
+#Load Opus
+discord.opus.load_opus('/usr/lib/x86_64-linux-gnu/libopus.so.0')
+if discord.opus.is_loaded() is False:
+    print('Opus did not load correctly')
+elif discord.opus.is_loaded() is True:
+    print('Opus loaded correctly')
+
+#Currently Playing
+currentPlaying = 'None'
+
+
+def bigTime():
+    """Get time in M/D/Y||H:M:S format."""
+    _bigTime = time.strftime('%D||%H:%M:%S')
+    return _bigTime
+
+
+# Suppress noise about console usage from errors
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+class Cmds(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def play(self, ctx, url):
+        """Streams from a url"""
+
+        global currentPlaying
+        channel = ctx.author.voice.channel
+
+        #Join channel if not already
+        if ctx.voice_client.is_connected() is False:
+            if ctx.voice_client is not None:
+                return await ctx.voice_client.move_to(channel)
+            await channel.connect()
+
+        if ctx.voice_client.is_playing() is True:
+            ctx.voice_client.stop()
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        currentPlaying = 'Now playing: {}'.format(player.title)
+        await ctx.send('Now playing: ' + currentPlaying)
+        print('Playing: {0} || {1}'.format(currentPlaying, bigTime()))
+
+    @commands.command()
+    async def pause(self, ctx):
+        """Pause/Unpause audio stream."""
+
+        if ctx.voice_client.is_paused() is False:
+            ctx.voice_client.pause()
+            print('Paused || {}'.format(bigTime()))
+        elif ctx.voice_client.is_paused() is True:
+            ctx.voice_client.resume()
+            print('Unpaused || {}'.format(bigTime()))
+
+    @commands.command()
+    async def leave(self, ctx):
+        """Stops and disconnects the bot from voice"""
+
+        currentPlaying = 'None'
+        await ctx.voice_client.disconnect()
+        print('Left voice || {}'.format(bigTime()))
+
+    @commands.command()
+    async def list(self, ctx):
+        """Lists the current song"""
+
+        if currentPlaying is not 'None':
+            await ctx.send('Currently playing: ' + currentPlaying)
+        else:
+            await ctx.send('Nothing is playing')
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send("Changed volume to {}%".format(volume))
+
+    @commands.command(name='help')
+    async def _help(self, ctx):
+        """Lists commands."""
+
+        await ctx.send("""Beepbeep commands:
+        b/play <url> - Plays a YouTube URL
+        b/pause - Pause/Unpause
+        b/leave - Disconnects the bot
+        b/list - List the current song
+        b/volume <volume> - Changes volume
+        """)
+
+    @play.before_invoke
+    async def ensure_voice(self, ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("b/"),
+                   description='Relatively simple music bot example')
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as {0} ({0.id}) || {1}'.format(bot.user, bigTime()))
+    print('------')
+bot.remove_command('help')
+bot.add_cog(Cmds(bot))
+bot.run('Add token here')
